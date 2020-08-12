4af398207761ffb47598db36cd5ccb3c8fa00025
Varun Madhavan <vmm221313@gmail.com>
2020-08-13 01:04:28 +0530

    Classification using Meta-data

--- main.py
+++ main.py
@@ -21,10 +21,11 @@ config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
 def fit_tokenizer(args, df):
-	df['Text'] = df['questionText'] + ' ' + df['answerText'] 
+	#df['Text'] = df['questionText'] + ' ' + df['answerText'] 
 
 	texts = []
-	texts += df['Text'].to_list()
+	texts += df['text'].to_list()
+	texts += df['meta'].to_list()
 
 	tokenizer = Tokenizer(num_words=args.vocab_size)
 	tokenizer.fit_on_texts(texts)
@@ -48,39 +49,59 @@ def make_embedding_layer(args, tokenizer):
 	return GloVe
 
 def get_train_val_test(args, df, tokenizer):
-	sequences = tokenizer.texts_to_sequences(df['Text'].to_list())
-	data = pad_sequences(sequences, maxlen=args.max_seq_len)
+	sequences 		= tokenizer.texts_to_sequences(df['text'].to_list())
+	meta 			= tokenizer.texts_to_sequences(df['meta'].to_list())
 
-	labels = df['Helpfulness_Target'].to_list()
-	labels = to_categorical(np.asarray(labels))	
+	data 			= pad_sequences(sequences, maxlen=args.max_seq_len)
+	meta 			= pad_sequences(meta, maxlen=args.max_seq_len)
 
-	X_train = data[:int(args.train_percent*len(data))]
-	X_val = data[int(args.train_percent*len(data)):int(args.val_percent*len(data))]
-	X_test = data[int(args.val_percent*len(data)):]
+	labels 			= df['target'].to_list()
+	labels 			= to_categorical(np.asarray(labels))	
 
-	y_train = labels[:int(args.train_percent*len(labels))]
-	y_val = labels[int(args.train_percent*len(labels)):int(args.val_percent*len(labels))]
-	y_test = labels[int(args.val_percent*len(labels)):]
+	stacked_data = []
+	for i in range(len(data)):
+		stacked_data.append(np.vstack((data[i], meta[i])))
+		
+	stacked_data = np.array(stacked_data)
 
-	return (X_train, y_train), (X_val, y_val), (X_test, y_test) 
+	X_train 		= stacked_data[:int(args.train_percent*len(stacked_data))].reshape(-1, args.max_seq_len, 2)
+	X_val 			= stacked_data[int(args.train_percent*len(stacked_data)):int(args.val_percent*len(stacked_data))].reshape(-1, args.max_seq_len, 2)
+	X_test			= stacked_data[int(args.val_percent*len(stacked_data)):].reshape(-1, args.max_seq_len, 2)
+
+	y_train 		= labels[:int(args.train_percent*len(labels))]
+	y_val 			= labels[int(args.train_percent*len(labels)):int(args.val_percent*len(labels))]
+	y_test 			= labels[int(args.val_percent*len(labels)):]
+
+	return (X_train, y_train), (X_val,  y_val), (X_test, y_test)
 
 def build_model(args, GloVe):
-	sequence_input = Input(shape=(args.max_seq_len,), dtype='int32')
-	
-	x = GloVe(sequence_input)
-	x = Reshape((50, 300, 1))(x)
+	sequence_input = Input(shape=(args.max_seq_len, 2), dtype='int32')
+
+	text_in = sequence_input[:, :, 0]
+	x = GloVe(text_in)
+	x = Reshape((350, 300, 1))(x)
 	x = Conv2D(256, 5, activation='tanh')(x)
 	x = MaxPooling2D((46, 296))(x)
 	x = Flatten()(x)
 	x = Dropout(0.2)(x)
-	preds = Dense(2, activation='softmax')(x)
+	
+	meta_in = sequence_input[:, :, 1]
+	m = GloVe(meta_in)
+	m = Reshape((350, 300, 1))(m)
+	m = Conv2D(256, 5, activation='tanh')(m)
+	m = MaxPooling2D((46, 296))(m)
+	m = Flatten()(m)
+	m = Dropout(0.2)(m)
+	
+	out = tf.keras.backend.concatenate((x, m), axis=1)
+	preds = Dense(2, activation='softmax')(out)
 
 	model = Model(sequence_input, preds)
-	optimizer = tf.keras.optimizers.Adam(lr=0.0001)
-
+	
+	optimizer = tf.keras.optimizers.Adam(lr=0.001)
 	model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-	print(model.summary())
+	model.summary()
 
 	return model
 
@@ -90,20 +111,14 @@ def main(args):
 	tokenizer = fit_tokenizer(args, df)
 	GloVe = make_embedding_layer(args, tokenizer)
 
-	(X_train, y_train), (X_val, y_val), (X_test, y_test) = get_train_val_test(args, df, tokenizer)
-		
-	#print(df['Helpfulness_Target'].value_counts())
-	
+	(X_train, y_train), (X_val,  y_val), (X_test, y_test) = get_train_val_test(args, df, tokenizer)
+
+	#'''
 	model = build_model(args, GloVe)
 
 	earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.001, patience=10)
-	
-	print(np.bincount(np.argmax(y_train, axis=1)))
-	print(np.bincount(np.argmax(y_val, axis=1)))
-	print(np.bincount(np.argmax(y_test, axis=1)))
 
-	#'''
-	model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=100, batch_size=32, callbacks=[earlystop], shuffle=True)
+	model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=100, batch_size=4, callbacks=[earlystop], shuffle=True)
 
 	y_pred = model.predict(X_test)
 	#print(y_pred[:100])
@@ -116,10 +131,10 @@ def main(args):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser() 
 	
-	parser.add_argument('-data_path', type=str, default='data/automative_question_answers.csv')
+	parser.add_argument('-data_path', type=str, default='data/Auto_meta_qar.csv')
 
 	parser.add_argument('-vocab_size', type=int, default=1000000)
-	parser.add_argument('-max_seq_len', type=int, default=50) #for default size -> average + 3 stds of lengths is 47
+	parser.add_argument('-max_seq_len', type=int, default=350) #for default size -> average + 3 stds of lengths is 350
 
 	parser.add_argument('-train_percent', type=float, default=0.7)
 	parser.add_argument('-val_percent', type=float, default=0.9)
